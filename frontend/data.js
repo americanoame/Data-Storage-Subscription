de63065a1c1f6d81bad7af615d43248e38c37c4f
Andre Silva <andrenations@gmail.com>
2024-12-29 23:23:35 -0500

    add the data

---
+++ frontend/data.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+      orther: "Basic",
+      personal:  "Basic personal use",
+      free: "Free",
+      price: "free",
+       
+    },
+    
+    {
+      name: "Startup",
+      commercial: "For commercial use",
+      monthlyPrice: 95,
+      yearlyPrice: 79,
+      discountedMonthlyPrice: 79,
+      discountedYearlyPrice: 948,
+      priceCute: "1140",
+      features: ["250MB Save Data", "30 Days Storage"],
+    },
+    {
+      name: "Standard",
+      suitable: "teams",
+      monthlyPrice: 375,
+      yearlyPrice: 299,
+      discountedMonthlyPrice: 299,
+      discountedYearlyPrice: 8388,
+      priceCute: "4500",
+      features: ["500MB Save Data", "Unlimited Storage"],
+      
+    },
+    {
+      name: "Professional",
+      enterprise: "Enterprises",
+      monthlyPrice: 875,
+      yearlyPrice: 1000,
+      discountedMonthlyPrice: 699,
+      discountedYearlyPrice: 3588,
+      priceCute: "10500",
+      features: ["1500MB Save Data", "Unlimited Storage"],
+      
+    },
+  ];
+
+  export default data;
